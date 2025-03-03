32ea753c445c1420566ce1b581690094ccfcd4d2
een <iankerr@IANKERR30F1>
2025-03-03 11:57:20 -0700

    Began work on reading pgn file

---
+++ ChessBrowser/Components/ChessGame.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+
+namespace ChessBrowser.Components
+{
+    public class ChessGame
+    {
+        public string whitePlayer { get; set; }
+        public string blackPlayer { get; set; }
+        public string eventName { get; set; }
+        public string site { get; set; }
+        public string round { get; set; }
+        public int whiteElo { get; set; }
+        public int blackElo { get; set; }
+        public string result { get; set; }
+        public string stringDate { get; set; }
+        public string moves { get; set; }
+
+        public ChessGame(string whitePlayer, string blackPlayer, string eventName, string site, string round, int whiteElo, int blackElo, string result, string stringDate, string moves)
+        {
+            this.whitePlayer = whitePlayer;
+            this.blackPlayer = blackPlayer;
+            this.eventName = eventName;
+            this.site = site;
+            this.round = round;
+            this.whiteElo = whiteElo;
+            this.blackElo = blackElo;
+            this.result = result;
+            this.stringDate = stringDate;
+            this.moves = moves;
+        }
+        public ChessGame()
+        {
+
+        }
+
+        public string printEvent()
+        {
+            return this.eventName;
+        }
+    }
+}
---
+++ ChessBrowser/Components/PGNParser.cs
@@ -0,0 +1,94 @@
+﻿
+using System.IO;
+using System;
+
+namespace ChessBrowser.Components
+{
+	public class PGNParser
+	{
+		public List<ChessGame> chessGames;
+		public PGNParser(string pgnFilePath)
+		{
+			chessGames = new List<ChessGame>();
+			using (StreamReader reader = new StreamReader(pgnFilePath))
+			{
+                ChessGame game = new ChessGame();
+                string line;
+				while ((line = reader.ReadLine()) != null)
+				{
+                    if (line.StartsWith('['))
+                        populateAtribute(line, game);
+  
+					if(line.StartsWith("1."))
+                    {
+                        string moves = "";
+                        string moveLine;
+                        while ((moveLine = reader.ReadLine()) != "")
+                            moves += moveLine;
+                        
+                        game.moves = moves;
+
+                        chessGames.Add(game);
+                        game = new ChessGame();
+                    }
+                   
+				}
+
+
+			}
+
+
+		}
+      
+		private void populateAtribute(string line, ChessGame game)
+		{
+			
+
+			string dataType = line.Substring(1, 10);
+            if (dataType.Contains("Event "))
+                game.eventName = getValue(line);
+            else if (dataType.Contains("Site"))
+                game.site = getValue(line);
+            else if (dataType.Contains("EventDate"))
+                game.stringDate = getValue(line);
+            else if (dataType.Contains("Round"))
+                game.round = getValue(line);
+            else if (dataType.Contains("White "))
+                game.whitePlayer = getValue(line);
+            else if (dataType.Contains("Black "))
+                game.blackPlayer = getValue(line);
+            else if (dataType.Contains("WhiteElo"))
+                game.whiteElo = Int32.Parse(getValue(line));
+            else if (dataType.Contains("BlackElo"))
+                game.blackElo = Int32.Parse(getValue(line));
+           
+
+
+
+        }
+
+		private string getValue(string line)
+		{
+
+            string dataType = line.Substring(1, 10);
+          
+                int index = 0;
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (line[i] == '"')
+                    {
+                        index = i;
+                        break;
+                    }
+                }
+
+                line = line.Substring(index + 1, line.Length - index - 3);
+
+            
+
+			return line;
+
+        }
+	}
+}
